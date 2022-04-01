eb6d9701e4c4d4d45d6380009466894ab26d995f
akulacka <a.kulacka@gmail.com>
2022-04-01 22:20:18 +0100

    second commit

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-file_number=14
+function response_reading {
+	echo "How many files are in the current directory?"
+	read response
+}
 
-echo "How many files are in the current directory?"
-read response
+file_number=$(ls | wc -l)
+response_reading
+while [[ ! $response -eq $file_number  ]]
+do
+	if [[ $response -gt $file_number ]]
+	then
+		echo "Your number is too high."
+	else
+		echo "Your number is too low."
+	fi
+	response_reading
+done
+echo "Congratulations!"
 
